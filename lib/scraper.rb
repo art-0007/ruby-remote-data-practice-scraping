e8371c1e7747c6c79ec823d82476b558e7a3e26f
Artur <info@aigroupusa.com>
2021-02-16 22:26:25 -0500

    commit tree c6675a5ad751586dadffb14d5a9a90b858e0889a

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
+#require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+doc.css(".headline-26OIBN").text
+
+
+# courses = doc.css(".title-oE5vT4")
+# html = open("https://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
+# courses.each do |course|
+#     puts course.text.strip
+# end 
+
+
+
+
+ 
